11d96946b9b649458d4061dcaf6caca5fdc385ba
kevingamez <ks.gamez@uniandes.edu.co>
2023-04-10 16:33:43 -0500

    Changes

--- Client/Client.py
+++ Client/Client.py
@@ -1,73 +1,67 @@
-# Importar librerias para el manejo del sistema, algoritmos de hash, tiempos, captura de paquetes, manejo de threads, manejo de sockets y funciones definidas en el archivo utils
 import socket
 import os
 import threading
 from Utils import write_log_file
 import time
 from datetime import datetime
-import pyshark
+
 
 # Constantes SEPARATOR, BUFFER_SIZE, HOST y PORT
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096
 BUFFER_SIZE_UDP = 30000
-HOST = '127.0.0.1'
+HOST = '192.168.152.131'
 PORT = 9879
 PORT_UDP = 12000
 
-# Contador del numero de threads ejecutandose
+
 contador = 0
 
-# Funcion que correr cada thread la cual consiste en recibir el archivo, el hash y la recepcion de confirmacion de integridad
-# Por cada thread que se corre se genera un archivo log que contiene la informacion respectiva detalla segun las especificaciones de la guia
 def correr_clientes(ClientSocket, ClientSocketUDP, ServerAddress):
-
-    # Contador global del numero de threads
     global contador 
     contador+=1
 
-    # Captura de paquetes por la interfaz de ethernet 
-    capture = pyshark.LiveCapture(interface='Ethernet')
-    capture.sniff(timeout=1)
-
-    # Tiempo de incio de ejecucion
     start_time = datetime.now()
 
-    # Entablar conexiones con el el servidor para empezar la transferencia del archivo
+
     try:
         ClientSocket.connect((HOST, PORT))
         mensaje = "listo"
         ClientSocket.send(str.encode(mensaje))
         received = ClientSocket.recv(BUFFER_SIZE).decode()
 
-        # Variables correspondientes al nombre del archivo y tamanio del archivo
         filename, filesize = received.split(SEPARATOR)
         filename = os.path.basename(filename)
         filesize = int(filesize)
 
         sent = ClientSocketUDP.sendto(b"Empezar", ServerAddress)
 
-        # Escritura del archivo recibido por partes desde el servidor
-        with open('./ArchivosRecibidos/'+ filename, "wb") as f:
+        with open( "./Client/ArchivosRecibidos/"+filename, "wb") as f:
             while True:
                 #print("2.1")
                 bytes_read, address = ClientSocketUDP.recvfrom(BUFFER_SIZE_UDP)
                 ClientSocketUDP.settimeout(60)
                 f.write(bytes_read)
 
+        
+
     except socket.error as e:
         print("Vamos bien")
-        # Tiempo final de ejecucion
         finish_time = datetime.now()
-
-        # Escritura del archivo log con la informacion establecida en la guia
-        numeropack=str(capture).replace("<LiveCapture (","").replace(" packets)>","")
-        write_log_file(filename, filesize, contador, "No se entrega el archivo exitosamente", start_time, finish_time, int(numeropack))
+        tiempo = finish_time - start_time
+        #write_log_file(filename, filesize, "No se entrega el archivo exitosamente", tiempo, contador )
 
         print("Un thread ha acabado:" + str(e))
         # Cerrar la conexion del socket
         ClientSocketUDP.close()
         ClientSocket.close()
+
+        
+    finish_time = datetime.now()
+    tiempo = finish_time - start_time
+    write_log_file(filename, filesize, "Se entrega exitosamente", tiempo, contador )
+    ClientSocketUDP.close()
+    ClientSocket.close()
     
 # Ingresar el numero de clientes a establecer conexion
 numClientes = int(input("Ingrese el número de clientes: "))
--- Client/Utils.py
+++ Client/Utils.py
@@ -3,14 +3,14 @@ import os
 import datetime
 
 # Funcion para generar el archivo de log en el directorio llamado Logs con la informacion determinada en la guia
-def write_log_file (file_name, file_size, num_client, status_file, start_time, finish_time, num_package):
+def write_log_file (file_name, file_size, status_file,time, num_package):
     today = datetime.datetime.now()
     name_file = str(today.year) + "-" + str(today.month) + "-" + str(today.day) + "-" + str(today.hour) + "-" + str(today.minute) + "-" + str(today.second) + "-" + str(today.microsecond) + str(file_name[7:8]) + "-log.txt"
-    log_file = open("./Logs/" + name_file, "w")
+    log_file = open("./Client/Logs/" + name_file, "w")
     log_file.write("Nombre del archivo: " + file_name + "\n")
-    log_file.write("Tamanio del archivo: " + str((os.path.getsize("./ArchivosRecibidos/"+file_name))) + "\n")
+    log_file.write("Tamanio del archivo: " + str((os.path.getsize("./Client/ArchivosRecibidos/"+file_name))) + "\n")
     log_file.write("Cliente que recibe la transeferencia de archivos: " + str(file_name[7:8]) + "\n")
     log_file.write("Estado de entrega del archivo: " + str(status_file) + "\n")
-    log_file.write("Tiempo de transferencia: " + str(finish_time - start_time) + "\n")
+    log_file.write("Tiempo de transferencia: " + str(time) + "\n")
     log_file.write("Numero de paquetes enviado: " + str(num_package) + "\n")
     log_file.write("Valor total de bytes enviado: " + str(int(file_size + (num_package * 8))) + "\n")
